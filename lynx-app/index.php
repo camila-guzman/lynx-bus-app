07f168c182a7c2495ffb7118822bc4c903be33fc
Camila Guzman <67128218+camila-guzman@users.noreply.github.com>
2021-11-07 18:20:47 -0500

    New setup
    Setting up new project files

---
+++ lynx-app/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+
+<!--include head section and navbar-->
+<?php include('templates/header.php'); ?>
+
+<!--Insert Content Here-->
+
+<!--include the footer with logo and hours-->
+<?php include('templates/footer.php'); ?>
+
+</html>
