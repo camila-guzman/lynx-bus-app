06e03da01bd98a05310fdfa6fcfd20c43e99e6bf
Camila Guzman <67128218+camila-guzman@users.noreply.github.com>
2021-11-09 00:52:32 -0500

    Routes page update
    Adding to routes page with function.
    
    Created route group with:
    link number, name, arrow linking to detail page, collapsible details area, and inbound/outbound toggle.

--- lynx-app/functions/routelistfunction.php
+++ lynx-app/functions/routelistfunction.php
@@ -19,10 +19,23 @@ function displayRoutes(){
        echo mysqli_error($sql);
     }
 
-    echo "<div class='route-group'>";
+    echo "<div class='route-list'>";
     while ($route = mysqli_fetch_assoc($result)){
-        echo "<div class='d-flex'><b>Link" . $route['LinkNumber'] . "::&nbsp;</b> <p>" . $route['Name'] . "</p></div>"; 
+        echo "<div class='route-group'><div class='d-flex align-items-baseline' type='button' data-bs-toggle='collapse' data-bs-target='#route-" . $route['LinkNumber'] . "-collapseDetails' aria-expanded='false' aria-controls='route-" . $route['LinkNumber'] . "-collapseDetails'>";
+
+        // display link number, name, and arrow with link
+        echo "<b>Link" . $route['LinkNumber'] . ":&nbsp;</b><p>" . $route['Name'] . "</p>"; 
+        echo "<a href='./route-detail-pages/link" . $route['LinkNumber'] . ".php' class='ml-auto'><i class='fas fa-arrow-right fa-lg pr-2'></i></a></div>";
+
+        // display route details collapsible section
+        echo "<div class='route-details collapse' id='route-" . $route['LinkNumber'] . "-collapseDetails'>
+        <div class='route-detail-area ml-5 p-3 mb-3'><div class='custom-control custom-switch p-0'>
+        <div class='d-flex justify-content-center align-items-star'>
+        <label class='inbound-label'>Inbound</label>
+        <input type='checkbox' class='custom-control-input' id='link" . $route['LinkNumber'] . "-toggle'><label class='custom-control-label' for='link" . $route['LinkNumber'] . "-toggle'>Outbound</label>
+        </div></div><p> this worked :'), need to work on getting the info to change on toggle</p></div></div></div>";
     }
+
     echo "</div>";
 
     if (!$result) {
--- lynx-app/routes.php
+++ lynx-app/routes.php
@@ -4,7 +4,6 @@ include ('functions/functions.php');
 include ('functions/routelistfunction.php');?>
 
 <!--Insert Content Here-->
-<div class="container">
     <h2 class="my-2">Routes</h2>
     <?php 
     
@@ -14,7 +13,6 @@ include ('functions/routelistfunction.php');?>
     
     closeDatabase();
     ?>
-</div>
 
 <!--include the footer with logo and hours-->
 <?php include('templates/footer.php'); ?>
