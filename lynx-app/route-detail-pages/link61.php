19669788f314bc2a6c88b24dbbbdd2890714e3fc
Camila Guzman <67128218+camila-guzman@users.noreply.github.com>
2021-11-09 00:53:19 -0500

    Create link61.php
    Adding new folder with route specific detail pages. Created link61.php to test with link 61 listing on routes.php

---
+++ lynx-app/route-detail-pages/link61.php
@@ -0,0 +1,11 @@
+<!--include head section and navbar-->
+<?php include('../templates/header.php'); 
+include('../functions/functions.php');
+include('../functions/routelistfunction.php');
+
+?>
+
+<p>Route 61 detail example</p>
+
+<!--include the footer-->
+<?php include('../templates/footer.php'); ?>
