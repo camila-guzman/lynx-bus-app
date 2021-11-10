a35b11e64b0146ef01b1707fc1fa902de26c23ad
reversed-quikz <transformersah@live.com>
2021-11-10 14:34:36 -0500

    fixed header when going into folders added settings stuff

--- lynx-app/settings.php
+++ lynx-app/settings.php
@@ -2,7 +2,22 @@
 <?php include('templates/header.php'); ?>
 
 <!--Insert Content Here-->
-
+<div>
+    <ul>
+<li>
+    <a href="/settings/language.php">Language</a>
+</li>
+<li>
+    <a href="/settings/accessibility.php">Accessibility</a>
+</li>
+<li>
+    <a href="/settings/paymentsettings.php">Payment Settings</a>
+</li>
+<li>
+    <a href="/settings/help.php">Help</a>
+</li>
+</ul>
+</div>
 
 <!--include the footer with logo and hours-->
 <?php include('templates/footer.php'); ?>
