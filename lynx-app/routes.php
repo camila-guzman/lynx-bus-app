a30f83d06148c89aee579fe79c4368e53868990d
Camila Guzman <67128218+camila-guzman@users.noreply.github.com>
2021-11-18 04:52:21 -0500

    Update routes.php
    - changed icon for filters
    - changed position for filter link
    - adding doctype

--- lynx-app/routes.php
+++ lynx-app/routes.php
@@ -1,10 +1,19 @@
-<!--include head section and navbar-->
+<!DOCTYPE html>
+<html>
+    <!--include head section and navbar-->
 <?php include('templates/header.php');
 include ('functions/functions.php');
 include ('functions/routelistfunction.php');?>
 
 <!--Insert Content Here-->
-    <h2 class="my-2">Routes</h2>
+<div class="d-flex align-items-baseline">
+    <h2 class="my-2 flex-grow-1">Routes</h2>
+    <div class="filter-button d-inline-flex align-items-center">
+        <!--<i class="fas fa-filter"></i>-->
+        <a href="filter.php">
+        <ion-icon name="options-outline" class="mr-2"></ion-icon>Filters</a>
+    </div>
+</div>
     <?php 
     
     openDatabase();
