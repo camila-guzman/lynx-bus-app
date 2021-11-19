f78c0e9e50954fadfba71e93a7d035bf5bb265b2
Camila Guzman <67128218+camila-guzman@users.noreply.github.com>
2021-11-19 15:59:30 -0500

    Update functions.php
    -adding more templates for filters
    - display function for filter results

--- lynx-app/functions/functions.php
+++ lynx-app/functions/functions.php
@@ -25,9 +25,77 @@ function closeDatabase()
     mysqli_close($connection);
 }
 
+/********************************************************************************* */
 // display button
 function displayBackButton($previousPage){
     echo "<div class='back-button d-inline-flex align-items-center my-2'>
         <i class='fas fa-arrow-left mr-2'></i><a href='" . $previousPage . "'>Back</a>
     </div>";
 }
+
+/********************************************************************************* */
+// display filter in submenu
+function filterSubmenu($filterName, $customId){
+
+    echo "<div class='list-item d-flex form-group mb-0'>
+       <p>" . $filterName . "</p>
+         <div class='custom-control custom-switch ml-auto d-flex'>
+            <input type='checkbox' class='custom-control-input' id='filterSwitch" . $customId ."' name='filter[]' value='" . $customId . "'>
+            <label class='custom-control-label' for='filterSwitch" . $customId ."'>On</label>
+        </div>
+    </div>";
+}
+
+// display last filter in submenu
+function filterLastSubmenu($filterName, $customId){
+
+    echo "<div class='last-list-item d-flex form-group mb-0'>
+       <p>" . $filterName . "</p>
+         <div class='custom-control custom-switch ml-auto d-flex'>
+            <input type='checkbox' class='custom-control-input' id='filterSwitch" . $customId ."' name='filter[]' value='" . $customId . "'>
+            <label class='custom-control-label' for='filterSwitch" . $customId ."'>On</label>
+        </div>
+    </div>";
+}
+
+// display badges for filters
+function displayBadgesFilters($filter){
+
+    global $arrayFiltersBadges;
+
+    echo "<span class='badge badge-primary mr-2 my-2'>" . $filter . "</span>";  
+
+}
+
+// create array of links with the filter that was selected
+// display as results
+function displayMultFilterResult($filter, $arrayFilterResult_LinkNumbers){
+
+    global $connection;
+
+    // query select routes with this filter
+    $filtersql = "SELECT LinkNumber FROM filters WHERE $filter = -1";
+
+    // run query
+    $filterResult = mysqli_query($connection, $filtersql);
+
+    echo mysqli_error($connection);
+
+    if ($filterResult === FALSE) {
+        echo "<p>FALSE</p>";
+        echo mysqli_error($filtersql);
+    }
+
+    while ($route = mysqli_fetch_array($filterResult)){
+        $link = $route['LinkNumber'];
+        if (!in_array($link, $arrayFilterResult_LinkNumbers)){
+            array_push($arrayFilterResult_LinkNumbers, $route['LinkNumber']);
+        }
+    }
+
+    foreach($arrayFilterResult_LinkNumbers as $value){
+        displayFilteredRouteList($value);
+    }
+
+}
+
