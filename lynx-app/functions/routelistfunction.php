800fdc7c5cc8caf673cb6bcb08991757f9431605
Camila Guzman <67128218+camila-guzman@users.noreply.github.com>
2021-11-19 15:57:05 -0500

    Update routelistfunction.php
    - global array for filter columns and display text
    - updating header style
    - adding filter route list function

--- lynx-app/functions/routelistfunction.php
+++ lynx-app/functions/routelistfunction.php
@@ -1,5 +1,25 @@
 <?php
 
+// array contains the filter with corresponding badge text
+// first parameter: table column name
+// second parameter: display text
+global $arrayFiltersBadges;
+
+$arrayFiltersBadges = array(
+    array("HalfHourService", "Every 30 minutes or better"), 
+    array("HourService", "Every 60 minutes"), 
+    array("ExpressService", "Express Service"), 
+    array("FastLinkService", "FastLink Service"), 
+    array("LYMMOService", "LYMMO Service"), 
+    array("Airports", "Airports"), 
+    array("DisneyLakeBuenaVista", "Disney World - LakeBuenaVista"), 
+    array("Downtown", "Downtown Orlando"), 
+    array("SunRail", "SunRail Connections"), 
+    array("Universal", "Universal Studios - International Drive"), 
+    array("UniversityColleges", "University and Colleges"), 
+    array("WinterPark", "Winter Park")
+);
+
 function displayDetailsPageHeader($connection, $link){
 
     // query select for routes list
@@ -16,7 +36,7 @@ function displayDetailsPageHeader($connection, $link){
         $link = $linkHeader['LinkNumber'];
         $name = $linkHeader['Name'];
 
-        echo "<h2>Link&nbsp" . $link . "</h2><h3>" . $name . "</h3>";
+        echo "<div class='link-header mt-2 mb-3'><h1 class='display-4'>Link&nbsp" . $link . "</h1><h2>" . $name . "</h2></div>";
     }
 }
 
@@ -31,21 +51,7 @@ function displayBadges($connection, $link){
         echo $sqlBadges;
     }
 
-    // array contains the filter with corresponding badge text
-    $arrayFiltersBadges = array(
-        array("HalfHourService", "Every 30 minutes or better"), 
-        array("HourService", "Every 60 minutes"), 
-        array("ExpressService", "Express Service"), 
-        array("FastLinkService", "FastLink Service"), 
-        array("LYMMOService", "LYMMO Service"), 
-        array("Airports", "Airports"), 
-        array("DisneyLakeBuenaVista", "Disney World - LakeBuenaVista"), 
-        array("Downtown", "Downtown Orlando"), 
-        array("SunRail", "SunRail Connections"), 
-        array("Universal", "Universal Studios - International Drive"), 
-        array("UniversityColleges", "University and Colleges"), 
-        array("WinterPark", "Winter Park")
-    );
+    global $arrayFiltersBadges;
 
     // setting number of filters
     $numfilters = 12;
@@ -171,8 +177,64 @@ function displayRouteList(){
     </div>";
 
     }
+}
 
+function displayFilteredRouteList($link){
 
+    global $connection;
+
+    // query select for routes list
+    $sql = "SELECT * FROM routes_list WHERE LinkNumber = $link";
+
+    // run query
+    $result = mysqli_query($connection, $sql);
+
+    echo mysqli_error($connection);
+
+    if ($result === FALSE) {
+        echo "<p>FALSE</p>";
+        echo mysqli_error($sql);
+    }
+
+    echo "<div class='route-list mb-2'>";
+    while ($route = mysqli_fetch_assoc($result)){
+
+        // helper variables
+        $link = $route['LinkNumber'];
+        $route_name = $route['Name'];
+
+        echo "<div class='route-group'><div class='d-flex align-items-baseline' type='button' data-bs-toggle='collapse' data-bs-target='#route-" . $link . "-collapseDetails' aria-expanded='false' aria-controls='route-" . $link . "-collapseDetails'>";
+
+        // display link number, name, and arrow with link
+        echo "<b>Link&nbsp;" . $link . ":&nbsp;</b><p>" . $route_name . "</p>"; 
+
+        /* link with arrow
+        echo "<a href='./route-detail-pages/link" . $link . ".php' class='ml-auto'><i class='fas fa-arrow-right fa-lg pr-2'></i></a></div>"; */
+        
+        // link with dropdown icon
+        echo "<a href='./route-detail-pages/link" . $link . ".php' class='ml-auto'><i class='fas fa-sort-down pr-2'></i></a></div>";
+
+        // display route details collapsible section
+        echo "<div class='route-details collapse ml-5' id='route-" . $link . "-collapseDetails'>";
+
+            // filter badges
+            echo "<div class='filter-badges mb-3'>";
+            displayBadges($connection, $link);
+            echo "</div>";
+
+            // in and outbound stops
+            displayStops($connection, $link);
+
+            //button style
+            echo "<a class='w-100 btn btn-lg btn-primary mb-4' href='./route-detail-pages/link" . $link . ".php'>View Route Details</a>";
+            
+            /*arrow style 
+            echo "<div class='d-flex justify-content-end align-items-baseline'><a class='mb-4 view-route-details' href='./route-detail-pages/link" . $link . ".php'>View Route Details</a><i class='fas fa-arrow-right fa-lg pr-2 ml-2'></i></div>"; */
+
+        echo "</div>
+    </div>";
+
+    }
 }
 
 ?>
