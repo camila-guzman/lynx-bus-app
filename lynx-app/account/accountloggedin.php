dfc66709e83065ed7d5ca9898f23abba2fe8dc2d
reversed-quikz <transformersah@live.com>
2021-11-19 18:37:51 -0500

    Update accountloggedin.php
    still not working but i can get it to show the page

--- lynx-app/account/accountloggedin.php
+++ lynx-app/account/accountloggedin.php
@@ -4,25 +4,36 @@
         </div>
         <div class = "col">
             <?php
+            session_start();
+            $id = $username = $firstname = $lastname = $password = "";
             global $connection;
             // open connection to database
             $connection = mysqli_connect("localhost", "jo889461", "!Throwaway123", "jo889461");
             if (!$connection){
                 echo "<p> not connecting</p>";
             }
-            $_SESSION["username"] = $usernamevar;  
-            echo "i got to here"; 
-           
-            $sql = sprintf("SELECT username, firstname, lastname, password FROM users WHERE username = $usernamevar");
-            $result = mysqli_query($sql);
-            echo "i got to here as well";
+            else {
+                $username = $_SESSION['username'];
+                echo $username;
+            $sql = "SELECT id, username, firstname, lastname, password FROM users WHERE username = $username";
 
-            while ($row = mysql_fetch_assoc($result)) {
-                echo $row['username'];
-                echo $row['firstname'];
-                echo $row['lastname'];
-                echo $row['password'];
+            $result = mysqli_query($connection, $sql);
+            if ($result === FALSE) {
+                echo "<p>FALSE</p>";
+                echo mysqli_error($connection);
             }
+            while ($finishedmaybe = mysqli_fetch_assoc($result)){
+                $id = $finishedmaybe['id'];
+                $username1 = $finishedmaybe['username'];
+                $firstname = $finishedmaybe['firstname'];
+                $lastname = $finishedmaybe['lastname'];
+                $password = $finishedmaybe['password'];
+                 echo $id;
+        
+            }
+
+            }
+           
             echo "wow here to huh";
             global $connection;
         // close connection
