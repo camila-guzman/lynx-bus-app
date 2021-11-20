4c07a1da27389be88b5bad105e566bd45c3d77c2
Camila Guzman <67128218+camila-guzman@users.noreply.github.com>
2021-11-19 22:29:43 -0500

    login path fix

--- lynx-app/account/accountloggedout.php
+++ lynx-app/account/accountloggedout.php
@@ -1,5 +1,5 @@
 
 <div class="text-center my-5">
 <h2> Unfortunately you are not logged in.</h2>
-<h2> Please <a href="../index.php" class="login-a">Log In</a> to continue!</h2>
+<h2> Please <a href="./index.php" class="login-a">Log In</a> to continue!</h2>
 </div>
