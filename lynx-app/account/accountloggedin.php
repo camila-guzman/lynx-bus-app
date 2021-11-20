e14de6ec8f64cc653c50251d66bf93b9df697179
reversed-quikz <transformersah@live.com>
2021-11-19 21:57:38 -0500

    got it to show stuff
    working on updating passwords

--- lynx-app/account/accountloggedin.php
+++ lynx-app/account/accountloggedin.php
@@ -34,11 +34,57 @@
           
 
             }
-           
-            global $connection;
-        // close connection
+            
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+        if(empty(trim($_POST["passwordnew"]))){
+            $password_err = "Please enter a password.";     
+        } elseif(strlen(trim($_POST["passwordnew"])) < 6){
+            $password_err = "Password must have atleast 6 characters.";
+        } else{
+            $passwordnew = trim($_POST["passwordnew"]);
+        }
+        
+        // Validate confirm password
+        if(empty(trim($_POST["confirm_password"]))){
+            $confirm_password_err = "Please confirm password.";     
+        } else{
+            $confirm_password = trim($_POST["confirm_password"]);
+            if(empty($password_err) && ($passwordnew != $confirm_password)){
+                $confirm_password_err = "Password did not match.";
+            }
+        }
+
+        if(empty($confirm_password_err)&& empty($password_err)){
+
+        $sql = "UPDATE users  SET password = '$param_password' WHERE username = '$username'";
+         
+        if($stmt = mysqli_prepare($connection, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "s", $param_password);
+            
+            
+            // Set parameters
+            $param_password = password_hash($passwordnew, PASSWORD_DEFAULT); // Creates a password hash
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                // Redirect to login page
+                header("location: ../account.php");
+            } else{
+                echo "Oops! Something went wrong. Please try again later.2";
+            }
+
+            // Close statement
+            mysqli_stmt_close($stmt);
+        }
+
+        }
         mysqli_close($connection);
-            ?>
+    }
+
+    ?>
+
         </div>  
 
         <div class="container light-style flex-grow-1 container-p-y">
@@ -57,33 +103,34 @@
 
               <hr class="border-light m-0">
 
-              <div class="card-body">
-                <div class="form-group">
-                  <label class="form-label">Username</label>
-                  <input type="text" class="form-control mb-1" value="<?php echo $username1;?>" readonly>
-                </div>
-                <div class="form-group">
-                  <label class="form-label">First Name</label>
-                  <input type="text" class="form-control" value="<?php echo $firstname;?>">
-                </div>
-                <div class="form-group">
-                  <label class="form-label">Last Name</label>
-                  <input type="text" class="form-control mb-1" value="<?php echo $lastname;?>">
-                </div>
-                <div class="form-group">
-                  <label class="form-label">Current password</label>
-                  <input type="password" class="form-control">
-                </div>
+              <div class="form-group">
+                <label>Username</label>
+                <input type="text" name="username" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>" readonly>
+                <span class="invalid-feedback"><?php echo $username_err; ?></span>
+            </div> 
+            <div class="form-group">
+                <label>First Name</label>
+                <input type="text" name="firstname" class="form-control <?php echo (!empty($firstname_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $firstname; ?>" readonly>
+                <span class="invalid-feedback"><?php echo $firstname_err; ?></span>
+            </div>
+            <div class="form-group">
+                <label>Last Name</label>
+                <input type="text" name="lastname" class="form-control <?php echo (!empty($lastname_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $lastname; ?>" readonly>
+                <span class="invalid-feedback"><?php echo $lastname_err; ?></span>
+            </div>   
+            <div class="form-group">
+                <label>New Password</label>
+                <input type="password" name="passwordnew" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $passwordnew; ?>">
+                <span class="invalid-feedback"><?php echo $password_err; ?></span>
+            </div>
 
-                <div class="form-group">
-                  <label class="form-label">New password</label>
-                  <input type="password" class="form-control">
-                </div>
+            <div class="form-group">
+                <label>Repeat New Password</label>
+                <input type="password" name="password_confirm" class="form-control <?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $confirm_password; ?>">
+                <span class="invalid-feedback"><?php echo $confirm_password_err; ?></span>
+            </div>
 
-                <div class="form-group">
-                  <label class="form-label">Repeat new password</label>
-                  <input type="password" class="form-control">
-              </div>
+               
 
             </div>
             
