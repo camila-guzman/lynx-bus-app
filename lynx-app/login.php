0bf30ea8efec88f5bea292f6175f502f837c5a68
Camila Guzman <67128218+camila-guzman@users.noreply.github.com>
2021-11-19 19:43:36 -0500

    Delete login.php

--- lynx-app/login.php
+++
@@ -1,8 +0,0 @@
-<!--include head section and navbar-->
-<?php include('templates/header.php'); ?>
-
-<!--Insert Content Here-->
-
-
-<!--include the footer with logo and hours-->
-<?php include('templates/footer.php'); ?>
